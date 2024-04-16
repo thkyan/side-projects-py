59ca1802cfca1e782ef8d44abacc4c8720acef4f
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2024-04-16 15:38:57 -0300

    Update .py

--- 01/.py
+++ 01/.py
@@ -1,21 +1,31 @@
 import random
 
-opcoes = ('pedra', 'papel', 'tesoura')
-jogador = None
-computador = random.choice(opcoes)
-while jogador not in opcoes:
-    jogador = input('escolha uma opção: (pedra, papel ou tesoura):')
+opcoes = ('pedra', 'papel', 'tesoura') 
+jogando = True
 
-print(f'jogador escolheu: {jogador}')
-print(f'computador escolheu: {computador}')
+while True:
 
-if jogador == computador:
-    print('deu empate!')
-elif jogador == 'pedra'and computador == 'tesoura':
-    print('você ganhouuu')
-elif jogador == 'papel' and computador == 'pedra':
-    print('você ganhouuu')
-elif jogador == 'tesoura' and computador == 'papel':
-    print('você ganhouuu')
-else:
-    print('você perdeuuu')
+    jogador = None
+    computador = random.choice(opcoes)
+
+    while jogador not in opcoes:
+        jogador = input('escolha uma opção: (pedra, papel ou tesoura):')
+
+        print(f'jogador escolheu: {jogador}')
+        print(f'computador escolheu: {computador}')
+
+        if jogador == computador:
+            print('deu empate!')
+        elif jogador == 'pedra'and computador == 'tesoura':
+            print('você ganhouuu')
+        elif jogador == 'papel' and computador == 'pedra':
+            print('você ganhouuu')
+        elif jogador == 'tesoura' and computador == 'papel':
+            print('você ganhouuu')
+        else:
+            print('você perdeuuu')
+
+    if not input('quer continuar? (s/n)').lower() == 's':
+        jogando = False
+
+print('obrigado por jogar!')
